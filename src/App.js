18e1afadf1de98e97f49d4effb81b9d850b1e7cd
BAEK DONGWOO <qorehddn123@naver.com>
2021-03-07 23:59:54 +0900

    router 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './components/page/LoginPage/LoginPage';
+import MainPage from './components/page/MainPage/MainPage';
 function App() {
-    return <div>초기화</div>;
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={MainPage} />
+                <Route exact path="/login" component={LoginPage} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/page/MainPage/MainPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainPage(props) {
+    return <div>메인페이지</div>;
+}
+
+export default MainPage;
