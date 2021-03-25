976601d0f9290ceb9e984bd230634cb84d32f14c
BAEK DONGWOO <qorehddn123@naver.com>
2021-03-26 02:16:02 +0900

    회원가입 아이디,비밀번호, 비밀번호확인, 닉네임

--- src/components/page/LoginPage/LoginPage.js
+++ src/components/page/LoginPage/LoginPage.js
@@ -19,6 +19,20 @@ function LoginPage(props) {
                 .string()
                 .min(8, '비밀번호는 최소 4자리 이상입니다.')
                 .required('필수 항목입니다.'),
+            // checkPassword: yup.string().required('필수 항목입니다.'),
+            checkPassword: yup
+                .string()
+                .min(8, '비밀번호는 최소 4자리 이상입니다.')
+                .oneOf([yup.ref('password'), null], '비밀번호와 같지않습니다.'),
+            phoneNumber: yup
+                .string()
+                .max(11, '최대 11자리입니다 ')
+                .required('- 없이 숫자만 입력해주세요.'),
+            nickname: yup
+                .string()
+                .min(2, '최소 2자리 이상 입니다 ')
+                .max(6, '최대 6자리 이하 입니다')
+                .required('필수 항목입니다.'),
         }),
     });
     const {
@@ -35,31 +49,36 @@ function LoginPage(props) {
         // setErrors,
     } = formik;
     const [passwordHide, setPasswordHide] = useState(true);
+    const [checkPasswordHide, setCheckPasswordHide] = useState(true);
     const onClickChange = (e, id) => {
         // e.preventDefault();
         if (id === 'password') {
             setPasswordHide(!passwordHide);
-        }
+        } else if (id === 'checkPassword')
+            setCheckPasswordHide(!checkPasswordHide);
     };
     return (
         <Container>
             <Logincontainer>
                 <h1>회원가입</h1>
-
+                {console.log(values.email.length)}
                 <form onSubmit={handleSubmit}>
                     <Formcontainer>
+                        {/*아이디*/}
                         <TextContainer>
+                            <TextLabel>아이디</TextLabel>
                             <Textbox>
                                 <Input
                                     type="email"
                                     value={values.email}
                                     onChange={handleChange('email')}
                                     style={{
-                                        border: !values.email
-                                            ? 'border: 1px solid #adaabf'
-                                            : !errors.email
-                                            ? '1px solid #00d8be'
-                                            : '1px solid #ff5631',
+                                        border:
+                                            values.email.length === 0
+                                                ? 'border: 1px solid #adaabf'
+                                                : !errors.email
+                                                ? '1px solid #00d8be'
+                                                : '1px solid #ff5631',
                                     }}
                                 />
                                 {values.email && !errors.email && (
@@ -70,9 +89,10 @@ function LoginPage(props) {
                                 )}
                             </Textbox>
                         </TextContainer>
+                        {/*비밀번호*/}
                         <TextContainer>
                             <ShowBox>
-                                <TextLabel>이메일</TextLabel>
+                                <TextLabel>비밀번호</TextLabel>
                                 <HideLabel
                                     onClick={(e) => {
                                         onClickChange(e, 'password');
@@ -87,7 +107,14 @@ function LoginPage(props) {
                                     type={passwordHide ? 'password' : 'text'}
                                     value={values.password}
                                     onChange={handleChange('password')}
-                                    style={{ padding: '12px 3rem' }}
+                                    style={{
+                                        padding: '12px 3rem',
+                                        border: !values.password
+                                            ? 'border: 1px solid #adaabf'
+                                            : !errors.password
+                                            ? '1px solid #00d8be'
+                                            : '1px solid #ff5631',
+                                    }}
                                 />
                                 {values.password && !errors.password && (
                                     <SuccessIcons className="far fa-check-circle" />
@@ -97,11 +124,77 @@ function LoginPage(props) {
                                 )}
                             </Textbox>
                         </TextContainer>
+                        {/*비밀번호 체크*/}
+                        <TextContainer>
+                            <ShowBox>
+                                <TextLabel>비밀번호 확인</TextLabel>
+                                <HideLabel
+                                    onClick={(e) => {
+                                        onClickChange(e, 'checkPassword');
+                                    }}
+                                >
+                                    {checkPasswordHide ? 'Hide' : 'Show'}
+                                </HideLabel>
+                            </ShowBox>
+                            <Textbox>
+                                <LockIcons className="fas fa-lock" />
+                                <Input
+                                    type={
+                                        checkPasswordHide ? 'password' : 'text'
+                                    }
+                                    value={values.checkPassword}
+                                    onChange={handleChange('checkPassword')}
+                                    style={{
+                                        padding: '12px 3rem',
+                                        border: !values.checkPassword
+                                            ? 'border: 1px solid #adaabf'
+                                            : !errors.checkPassword
+                                            ? '1px solid #00d8be'
+                                            : '1px solid #ff5631',
+                                    }}
+                                />
+                                {values.checkPassword &&
+                                    !errors.checkPassword && (
+                                        <SuccessIcons className="far fa-check-circle" />
+                                    )}
+                                {values.checkPassword &&
+                                    errors.checkPassword && (
+                                        <FailIcons className="far fa-times-circle" />
+                                    )}
+                            </Textbox>
+                        </TextContainer>
+                        {/*닉네임*/}
+                        <TextContainer>
+                            <TextLabel>닉네임</TextLabel>
+                            <Textbox>
+                                <Input
+                                    type="email"
+                                    value={values.nickname}
+                                    onChange={handleChange('nickname')}
+                                    style={{
+                                        border: !values.nickname
+                                            ? 'border: 1px solid #adaabf'
+                                            : !errors.nickname
+                                            ? '1px solid #00d8be'
+                                            : '1px solid #ff5631',
+                                    }}
+                                />
+                                {values.nickname && !errors.nickname && (
+                                    <SuccessIcons className="far fa-check-circle" />
+                                )}
+                                {values.nickname && errors.nickname && (
+                                    <FailIcons className="far fa-times-circle" />
+                                )}
+                            </Textbox>
+                        </TextContainer>
+                        {/*버튼*/}
                         <button type="submit" onClick={handleSubmit}>
                             Login now
                         </button>
                         {errors.password && <p>{errors.password}</p>}
                         {errors.email && <p>{errors.email}</p>}
+                        {errors.checkPassword && <p>{errors.checkPassword}</p>}
+                        {errors.nickname && <p>{errors.nickname}</p>}
                     </Formcontainer>
                 </form>
             </Logincontainer>
@@ -114,7 +207,6 @@ export default LoginPage;
 const Container = styled.div``;
 const Formcontainer = styled.form`
     width: 100%;
-    background: yellow;
 `;
 const TextContainer = styled.div`
     margin: auto;
