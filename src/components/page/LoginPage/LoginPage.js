5d4f2c4b566f5ddfd5520c3782fbc6565cc1f8aa
BAEK DONGWOO <qorehddn123@naver.com>
2021-03-26 01:50:52 +0900

    회원가입 페이지 구현

--- src/components/page/LoginPage/LoginPage.js
+++ src/components/page/LoginPage/LoginPage.js
@@ -1,19 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
 import styled from 'styled-components';
-import {
-    fade,
-    ThemeProvider,
-    withStyles,
-    makeStyles,
-    createMuiTheme,
-} from '@material-ui/core/styles';
-import InputBase from '@material-ui/core/InputBase';
-import InputLabel from '@material-ui/core/InputLabel';
-import TextField from '@material-ui/core/TextField';
-import FormControl from '@material-ui/core/FormControl';
-import { Link as LinkS } from 'react-scroll';
 
 function LoginPage(props) {
     const formik = useFormik({
@@ -46,41 +34,147 @@ function LoginPage(props) {
         // resetForm,
         // setErrors,
     } = formik;
+    const [passwordHide, setPasswordHide] = useState(true);
+    const onClickChange = (e, id) => {
+        // e.preventDefault();
+        if (id === 'password') {
+            setPasswordHide(!passwordHide);
+        }
+    };
     return (
-        <div style={{ width: '100%', textAlign: 'center' }}>
-            {console.log(values.email)}
-            <div>
-                <h1>Join the game</h1>
-                <h2>Go inside the best gamers social network!</h2>
-                <form onSubmit={handleSubmit}>
-                    <Textbox>
-                        <input
-                            type="email"
-                            value={values.email}
-                            onChange={handleChange('email')}
-                        />
-                    </Textbox>
-                    <Textbox>
-                        <input
-                            type="password"
-                            value={values.password}
-                            onChange={handleChange('password')}
-                        />
-                    </Textbox>
+        <Container>
+            <Logincontainer>
+                <h1>회원가입</h1>
 
-                    <button type="submit" onClick={handleSubmit}>
-                        Login now
-                    </button>
-                    {errors.password && <p>{errors.password}</p>}
-                    {errors.email && <p>{errors.email}</p>}
+                <form onSubmit={handleSubmit}>
+                    <Formcontainer>
+                        <TextContainer>
+                            <Textbox>
+                                <Input
+                                    type="email"
+                                    value={values.email}
+                                    onChange={handleChange('email')}
+                                    style={{
+                                        border: !values.email
+                                            ? 'border: 1px solid #adaabf'
+                                            : !errors.email
+                                            ? '1px solid #00d8be'
+                                            : '1px solid #ff5631',
+                                    }}
+                                />
+                                {values.email && !errors.email && (
+                                    <SuccessIcons className="far fa-check-circle" />
+                                )}
+                                {values.email && errors.email && (
+                                    <FailIcons className="far fa-times-circle" />
+                                )}
+                            </Textbox>
+                        </TextContainer>
+                        <TextContainer>
+                            <ShowBox>
+                                <TextLabel>이메일</TextLabel>
+                                <HideLabel
+                                    onClick={(e) => {
+                                        onClickChange(e, 'password');
+                                    }}
+                                >
+                                    {passwordHide ? 'Hide' : 'Show'}
+                                </HideLabel>
+                            </ShowBox>
+                            <Textbox>
+                                <LockIcons className="fas fa-lock" />
+                                <Input
+                                    type={passwordHide ? 'password' : 'text'}
+                                    value={values.password}
+                                    onChange={handleChange('password')}
+                                    style={{ padding: '12px 3rem' }}
+                                />
+                                {values.password && !errors.password && (
+                                    <SuccessIcons className="far fa-check-circle" />
+                                )}
+                                {values.password && errors.password && (
+                                    <FailIcons className="far fa-times-circle" />
+                                )}
+                            </Textbox>
+                        </TextContainer>
+                        <button type="submit" onClick={handleSubmit}>
+                            Login now
+                        </button>
+                        {errors.password && <p>{errors.password}</p>}
+                        {errors.email && <p>{errors.email}</p>}
+                    </Formcontainer>
                 </form>
-            </div>
-        </div>
+            </Logincontainer>
+        </Container>
     );
 }
 
 export default LoginPage;
-const Formcontainer = styled.form``;
-const Textbox = styled.div`
+
+const Container = styled.div``;
+const Formcontainer = styled.form`
+    width: 100%;
+    background: yellow;
+`;
+const TextContainer = styled.div`
+    margin: auto;
+    max-width: 448px;
+    text-align: left;
     margin-top: 1rem;
+`;
+const Textbox = styled.div`
+    margin: auto;
+    max-width: 448px;
+    text-align: left;
+    display: flex;
+    position: relative;
+`;
+const Input = styled.input`
+    width: 100%;
+    height: 3rem;
+    border: 1px solid #adaabf;
+    box-sizing: border-box;
+    box-shadow: 0px 1px 2px rgba(50, 43, 95, 0.08);
+    border-radius: 4px;
+    padding: 12px 1.5rem;
+    &:focus {
+        outline: none;
+        border: 1px solid blue;
+        //border: 1px solid #00d8be;
+    }
+`;
+
+const Logincontainer = styled.div`
+    margin: auto;
+    max-width: 1250px;
+    text-align: center;
+`;
+const TextLabel = styled.p`
+    margin-bottom: 3px;
+`;
+const HideLabel = styled(TextLabel)`
+    cursor: pointer;
+`;
+const ShowBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+const SuccessIcons = styled.i`
+    position: absolute;
+    right: 12px;
+    top: 16px;
+    color: #00d8be;
+`;
+const FailIcons = styled.i`
+    position: absolute;
+    right: 12px;
+    top: 16px;
+    color: #ff5631;
+`;
+const LockIcons = styled.i`
+    position: absolute;
+    left: 16px;
+    top: 16px;
+    color: #adaabf;
 `;
