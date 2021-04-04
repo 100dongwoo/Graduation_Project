df842f4ec0303c89a78efc373eb792c48aef57c9
BAEK DONGWOO <qorehddn123@naver.com>
2021-04-04 16:27:06 +0900

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import store from './Redux/store';
 import { Provider } from 'react-redux';
+import axios from 'axios';
+
+if (process.env.NODE_ENV === 'production') {
+    axios.defaults.baseURL = 'http://3.35.167.116:8000/v1/';
+}
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
--- src/settings/url.js
+++ src/settings/url.js
@@ -1,10 +1,20 @@
+// let url = 'http://localhost';
+// url = 'http://3.35.167.116';
+// url = 'http://www.dorestory.com';
+//
+// if (process.env.NODE_ENV === 'production') {
+//     url = 'http://www.dorestory.com:8000/v1/';
+// }
+//
+// export const BASE_URL = url + ':8000/v1/';
+// // http://3.35.167.116:8000/v1/users/sign-up/
+
 let url = 'http://localhost';
 url = 'http://3.35.167.116';
 url = 'http://www.dorestory.com';
 
 if (process.env.NODE_ENV === 'production') {
-    url = 'http://www.dorestory.com:8000/v1/';
+    url = 'http://www.dorestory.com';
 }
 
-export const BASE_URL = url + ':8000/v1/';
-// http://3.35.167.116:8000/v1/users/sign-up/
+export const BASE_URL = url + ':8000/api/v1/';
