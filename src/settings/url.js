c7d39f6c2186717fbcacec0f3c4f1ceaaf91741b
BAEK DONGWOO <qorehddn123@naver.com>
2021-04-04 16:07:59 +0900

    test url

--- src/settings/url.js
+++ src/settings/url.js
@@ -3,7 +3,7 @@ url = 'http://3.35.167.116';
 url = 'http://www.dorestory.com';
 
 if (process.env.NODE_ENV === 'production') {
-    url = 'http://www.dorestory.com';
+    url = 'http://www.dorestory.com:8000/v1/';
 }
 
 export const BASE_URL = url + ':8000/v1/';
