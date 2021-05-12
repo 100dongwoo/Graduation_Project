ad953b63b46f0f834138f786f0a7737258ee65e4
BAEK DONGWOO <qorehddn123@naver.com>
2021-05-12 20:19:44 +0900

    toolkit

--- src/pages/Help/Help.js
+++ src/pages/Help/Help.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Chatting from './Chatting/Chatting';
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 function Help(props) {
     const [isHelp, setIsHelp] = useState(false);
+    const [isTooltipActive, setIsTooltipActive] = useState(false);
+    function sleep(ms) {
+        const wakeUpTime = Date.now() + ms;
+        while (Date.now() < wakeUpTime) {}
+    }
+    const showTooltip = () => {
+        setIsTooltipActive(true);
+    };
+    const hideTooltip = () => {
+        // sleep(2000);
+        setIsTooltipActive(false);
+    };
     const onHandleHelp = (e) => {
         setIsHelp(!isHelp);
     };
     return (
         <Container>
-            <HelpBox>
+            <HelpBox onMouseEnter={showTooltip} onMouseLeave={hideTooltip}>
                 <Social
                     className={
                         isHelp
@@ -20,10 +31,36 @@ function Help(props) {
                     onClick={onHandleHelp}
                 />
             </HelpBox>
+
             {isHelp && <Chatting />}
+            {isTooltipActive && !isHelp && (
+                <ToolTipbox>
+                    실시간 채팅을 확인 해보세요! &nbsp;
+                    <Icon className="far fa-grin-hearts" />
+                </ToolTipbox>
+            )}
         </Container>
     );
 }
+const Icon = styled.i`
+    font-size: 14px;
+`;
+const ToolTipbox = styled.div`
+    position: absolute;
+    bottom: 20px;
+    right: calc(100% + 10px);
+    width: 128px;
+    height: 3.2rem;
+    padding: 6px 10px;
+    font-size: 14px;
+    word-break: keep-all;
+    color: #222;
+    border-radius: 6px;
+    letter-spacing: -0.5px;
+    background-color: #fff;
+    -webkit-box-shadow: 0 0 30px 0 rgb(0 0 0 / 20%);
+    box-shadow: 0 0 30px 0 rgb(0 0 0 / 20%);
+`;
 // const Container = styled.div`
 //     position: fixed;
 //     right: 12px;
