3bb65815e7b1e66ca41e3d1e6aed8a59f12a3249
BAEK DONGWOO <qorehddn123@naver.com>
2021-05-14 21:08:51 +0900

    chat ui

--- src/pages/Help/Chatting/Comment.js
+++ src/pages/Help/Chatting/Comment.js
@@ -7,34 +7,38 @@ function Comment({ chat }) {
         <Container>
             <UserName>{chat?.user}</UserName>
             <ChatContent>{chat?.content}</ChatContent>
-
-            <Time>{moment().format('LT')}</Time>
+            <Time>{moment().locale('en').format('LT')}</Time>
         </Container>
     );
 }
 const Time = styled.p`
     font-size: 12px;
     margin: auto 0;
+    white-space: nowrap;
+    color: rgba(34, 34, 34, 0.5);
 `;
-const ChatContent = styled.span`
+const ChatContent = styled.p`
     font-size: 12px;
     word-break: break-all;
+    width: 100%;
+    margin: auto 6px;
 `;
-const UserName = styled.span`
+const UserName = styled.p`
     word-break: keep-all;
-    margin-right: 6px;
+    //margin-right: 16px;
     font-weight: 400;
     color: #12c0f9;
+    margin: auto 0;
 `;
 const Container = styled.div`
-    padding: 0 1rem;
+    padding: 0.1rem 0.8rem;
     display: flex;
-    //justify-content: space-between;
+    justify-content: space-between;
     height: 88px;
     box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.1);
     border-radius: 14px;
     background: #ffffff;
     margin-bottom: 2px;
-    align-items: center;
+    align-items: center; ;
 `;
 export default Comment;
