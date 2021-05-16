90100bcba6c21803fb8f10f2c4ad20fde73d7052
BAEK DONGWOO <qorehddn123@naver.com>
2021-05-16 16:58:34 +0900

    test'

--- src/pages/MainPage/CharacterPage/Character.js
+++ src/pages/MainPage/CharacterPage/Character.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import './css.css';
 
 function Character(props) {
-    return <Container id="character">{/*<Content></Content>*/}</Container>;
+    const onClickStart = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <Container id="character">
+            {
+                /*<Content></Content>*/
+                <figure className="snip1445">
+                    <img
+                        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample84.jpg"
+                        alt="sample84"
+                    />
+                    <figcaption>
+                        <GameStartContainer onClick={onClickStart}>
+                            <h2>게임시작</h2>
+                            <h4>START</h4>
+                        </GameStartContainer>
+                    </figcaption>
+                    {/*<a href="#"></a>*/}
+                </figure>
+            }
+        </Container>
+    );
 }
+const GameStartContainer = styled.div`
+    cursor: pointer;
+`;
 const Container = styled.div`
     max-width: 1250px;
     margin: auto;
