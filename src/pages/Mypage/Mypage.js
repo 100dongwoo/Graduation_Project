08bb55010c5b6ce053f69cc45954fb289c75d1c6
BAEK DONGWOO <qorehddn123@naver.com>
2021-05-23 20:58:05 +0900

    마이페이지 샘플데이터 생성

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { SamplePost } from '../../SampleData/SampleData';
+import styled from 'styled-components';
 
 function Mypage(props) {
     return <div>mypage</div>;
 }
-
+const Container = styled.div`
+    max-width: 1250px;
+    margin: auto;
+`;
 export default Mypage;
