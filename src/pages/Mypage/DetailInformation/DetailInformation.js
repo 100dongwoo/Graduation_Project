bdf1081c45df2266e7af51df9d5937d64014323f
BAEK DONGWOO <qorehddn123@naver.com>
2021-05-30 18:46:47 +0900

    내정보 페이지

--- src/pages/Mypage/DetailInformation/DetailInformation.js
+++ src/pages/Mypage/DetailInformation/DetailInformation.js
@@ -1,7 +1,72 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Table from 'react-bootstrap/Table';
+import styled from 'styled-components';
 
 function DetailInformation(props) {
-    return <div>내 정보asdsad</div>;
+    const auth = useSelector((state) => state.auth);
+    const { user } = auth;
+    return (
+        <div>
+            {console.log(user)}
+            <Table bordered hover size="md">
+                {/*<thead>*/}
+                <TR>
+                    <TableTitleTH>이메일</TableTitleTH>
+                    <TableContentTD>{user?.login_id}</TableContentTD>
+                    <TableTitleTH>유저 닉네임</TableTitleTH>
+                    <TableContentTD>{user?.nickname}</TableContentTD>
+                </TR>
+                {/*</thead>*/}
+                {/*<tbody>*/}
+                <TR>
+                    <TableTitleTH>레벨</TableTitleTH>
+                    <TableContentTD>{user?.level}</TableContentTD>
+                    <TableTitleTH>HP</TableTitleTH>
+                    <TableContentTD>{user?.hp}</TableContentTD>
+                </TR>
+                <TR>
+                    <TableTitleTH>공격력</TableTitleTH>
+                    <TableContentTD>{user?.damage}</TableContentTD>
+                    <TableTitleTH>방어력</TableTitleTH>
+                    <TableContentTD>{user?.defence}</TableContentTD>
+                </TR>
+                <TR>
+                    <TableTitleTH>점프력</TableTitleTH>
+                    <TableContentTD>LV. {user?.jump}</TableContentTD>
+                    {/*<td colSpan="2">Larry the Bird</td>*/}
+                    <TableTitleTH>스피드</TableTitleTH>
+                    <TableContentTD>LV. {user?.speed}</TableContentTD>
+                </TR>
+                {/*</tbody>*/}
+            </Table>
+        </div>
+    );
 }
-
+const TR = styled.tr`
+    border: 0;
+    font-family: 'NanumBarunGothic', 'Malgun Gothic', sans-serif;
+    //margin: auto 0;
+    //align-items: center;
+`;
+const TableTitleTH = styled.th`
+    color: #3776c7;
+    line-height: 49px;
+    height: 49px;
+    background-color: #f5f7fc;
+    border-left: 1px solid #cfd8e7;
+    border-top: 1px solid #cfd8e7;
+    text-align: left;
+    width: 25%;
+`;
+const TableContentTD = styled.th`
+    width: 25%;
+    font-size: 15px;
+    color: #333;
+    text-align: left;
+    line-height: 49px;
+    height: 49px;
+    background-color: #fff;
+    border-top: 1px solid #cfd8e7;
+`;
 export default DetailInformation;
