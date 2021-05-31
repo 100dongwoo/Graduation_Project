4a9a7d73ceae3bb978c51cbd1b98f384a9d067ae
BAEK DONGWOO <qorehddn123@naver.com>
2021-05-31 15:00:37 +0900

    마이페에지 렌더링 변수추가

--- src/pages/Mypage/Achievements/Achievements.js
+++ src/pages/Mypage/Achievements/Achievements.js
@@ -10,7 +10,9 @@ function Achievements({ clear }) {
     const [achievements, setAchievements] = useState([]);
     const [page, setPage] = useState(1); //현재 페이지
     const [totalPage, setTotalPage] = useState(1); //  전체 크기
+    const [totalCount, setTotalCount] = useState(null); //게시글의 총 갯수
     const auth = useSelector((state) => state.auth);
+    const [isReady, setIsReady] = useState(false);
     const { user } = auth;
     useEffect(() => {
         FetchQAchievement(1);
@@ -32,13 +34,15 @@ function Achievements({ clear }) {
                 }
 
                 setAchievements(res.data.results);
+                setTotalCount(res.data?.count);
                 setTotalPage(
-                    parseInt(res.data.count) !== 0
-                        ? parseInt(res.data.count) % 4 === 0
-                            ? parseInt(res.data.count / 4)
-                            : parseInt(res.data.count / 4) + 1
+                    parseInt(res.data?.count) !== 0
+                        ? parseInt(res.data?.count) % 20 === 0
+                            ? parseInt(res.data?.count / 20)
+                            : parseInt(res.data?.count / 20) + 1
                         : 1
                 );
+                setIsReady(true);
             })
             .catch((err) => {
                 if (err.response.data.msg) {
@@ -49,26 +53,57 @@ function Achievements({ clear }) {
             });
     };
     return (
-        <Container>
-            <MenuContainer>
-                {<h1>{clear ? '업적' : '미완료 업적'}</h1>}
-            </MenuContainer>
-            {achievements?.map((achievement) => (
-                <AchievementPost post={achievement} key={achievement.id} />
-            ))}
-            <PaginationComponent
-                totalPage={totalPage}
-                page={page}
-                handleChange={handleChange}
-            />
-        </Container>
+        <>
+            <Container>
+                <MenuContainer>
+                    {<h1>{clear ? '업적' : '미완료 업적'}</h1>}
+                </MenuContainer>
+                {achievements?.map((achievement) => (
+                    <AchievementPost post={achievement} key={achievement.id} />
+                ))}
+                {totalCount !== 0 ? (
+                    <PaginationComponent
+                        totalPage={totalPage}
+                        page={page}
+                        handleChange={handleChange}
+                    />
+                ) : clear ? (
+                    <NoPostFont>
+                        <UserName>'{user?.nickname}' </UserName> &nbsp; 님의
+                        완료 업적이 없습니다.
+                    </NoPostFont>
+                ) : (
+                    <NoPostFont>
+                        <UserName>'{user?.nickname}' </UserName> &nbsp; 님은
+                        모든 업적을 완료하셨습니다
+                    </NoPostFont>
+                )}
+            </Container>
+        </>
     );
 }
 
 export default Achievements;
+const UserName = styled.p`
+    color: #3776c7;
+    font-size: 1.9rem;
+    margin-top: 2px;
+    height: 100%;
+`;
+const NoPostFont = styled.span`
+    color: black;
+    font-size: 2rem;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background: #f9f9f9;
+    text-align: center;
+    padding-top: 3px;
+`;
 const Container = styled.div`
     margin-bottom: 3rem;
 `;
 const MenuContainer = styled.div`
     border-bottom: 4px solid #dedede;
+    margin-bottom: 1rem;
 `;
--- src/pages/Mypage/Myposts/Myposts.js
+++ src/pages/Mypage/Myposts/Myposts.js
@@ -11,6 +11,7 @@ function Myposts(props) {
     const [posts, setPosts] = useState([]);
     const [totalPage, setTotalPage] = useState(1);
     const [page, setPage] = useState(1); //현재 페이지
+    const [totalCount, setTotalCount] = useState(null); //게시글의 총 갯수
     const auth = useSelector((state) => state.auth);
     const { user } = auth;
     const handleChange = (e, value) => {
@@ -35,9 +36,10 @@ function Myposts(props) {
                 //     return;
                 // }
                 setPosts(res.data?.results);
+                setTotalCount(res.data?.count);
                 setTotalPage(
                     parseInt(res.data?.count) !== 0
-                        ? parseInt(res.data.count) % 20 === 0
+                        ? parseInt(res.data?.count) % 20 === 0
                             ? parseInt(res.data?.count / 20)
                             : parseInt(res.data?.count / 20) + 1
                         : 1
@@ -61,13 +63,33 @@ function Myposts(props) {
                     page={page}
                 />
             ))}
-            <PaginationComponent
-                totalPage={totalPage}
-                page={page}
-                handleChange={handleChange}
-            />
+            {totalCount !== 0 ? (
+                <PaginationComponent
+                    totalPage={totalPage}
+                    page={page}
+                    handleChange={handleChange}
+                />
+            ) : (
+                <NoPostFont>
+                    <UserName>'{user?.nickname}' </UserName> &nbsp;님의 작성된
+                    게시글이 없습니다
+                </NoPostFont>
+            )}
         </div>
     );
 }
-
+const UserName = styled.p`
+    color: #3776c7;
+    font-size: 1.9rem;
+    margin-top: 2px;
+    height: 100%;
+`;
+const NoPostFont = styled.span`
+    color: black;
+    font-size: 2rem;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background: #f9f9f9; ;
+`;
 export default Myposts;
