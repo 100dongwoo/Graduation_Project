53787b07d2bf2a41951a65b16a9682ee3cdf12b4
BAEK DONGWOO <qorehddn123@naver.com>
2021-06-01 13:22:39 +0900

    test build

--- src/settings/api.js
+++ src/settings/api.js
@@ -1,26 +1,26 @@
-import {create} from 'apisauce'
-import {BASE_URL} from './url'
+import { create } from 'apisauce';
+import { BASE_URL } from './url';
 
 const api = create({
     baseURL: BASE_URL,
     withCredentials: true,
-})
+});
 
-export const extraApi = create({baseURL: ''})
+export const extraApi = create({ baseURL: '' });
 
 api.addResponseTransform((response) => {
-    console.log(response)
+    console.log(response);
     if (response.status && response.status >= 500) {
         // eslint-disable-next-line no-throw-literal
-        throw 'Server Error'
+        throw 'Server Error';
     } else if (!response.status) {
         // eslint-disable-next-line no-throw-literal
-        throw 'API Error'
+        throw 'API Error';
     }
-})
+});
 
 api.addRequestTransform((request) => {
-    console.log(request)
-})
+    console.log(request);
+});
 
-export default api
+export default api;
