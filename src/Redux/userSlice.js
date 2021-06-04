357301ee1ee47ae46005e9fca55badfe387846f6
BAEK DONGWOO <qorehddn123@naver.com>
2021-06-04 13:24:57 +0900

    Testbuild

--- src/Redux/userSlice.js
+++ src/Redux/userSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const userSlice = createSlice({
     name: 'auth',
     initialState: {
-        user: {},
+        user: null,
     },
     reducers: {
         LOGIN: (state, action) => {
